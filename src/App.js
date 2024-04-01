24cfdfbf72afd6cbb3dc6fa5b75f5a89d996e894
Aiman-Hws <aiman.houass@gmail.com>
2024-04-01 03:39:08 +0000

    finished all the inputs

--- src/App.js
+++ src/App.js
@@ -11,11 +11,16 @@ function App() {
     email: '',
     phone: '',
     gender: '',
-    subject: ''
+    subject: '',
+    resume: null,
+    url: '',
+    choice: '',
+    about: ''
   });
 
   const formSubmit = (e) => {
     e.preventDefault();
+
     const newContact = { ...formData };
     setContacts([...contacts, newContact]);
     setFormData({
@@ -24,7 +29,11 @@ function App() {
       email: '',
       phone: '',
       gender: '',
-      subject: ''
+      subject: '',
+      resume: null,
+      url: '',
+      choice: '',
+      about: ''
     });
   }
 
@@ -37,7 +46,18 @@ function App() {
   }
 
   const reset = () => {
-
+    setFormData({
+      firstName: '',
+      lastName: '',
+      email: '',
+      phone: '',
+      gender: '',
+      subject: '',
+      resume: null,
+      url: '',
+      choice: '',
+      about: ''
+    });
   }
 
   return (
@@ -47,26 +67,38 @@ function App() {
       <div class="col-md-6">
         <form onSubmit={formSubmit}>
           <h1>Form in React</h1>
+          {/* First Name */}
           <div class="mb-3">
             <label htmlFor="firstName" className="form-label">First Name*</label>
-            <input type="text" className="form-control" name="firstName" id="firstName" placeholder="Enter First Name" 
-                    value={formData.firstName} onChange={handleChange} required/>
+            <input type="text" 
+                  className='form-control'
+                  name="firstName" 
+                  id="firstName" 
+                  placeholder="Enter First Name" 
+                  value={formData.firstName} 
+                  onChange={handleChange} 
+                  required
+            />
           </div>
+          {/* Last Name */}
           <div class="mb-3">
             <label htmlFor="lastName" className="form-label">Last Name*</label>
             <input type="text" className="form-control" name="lastName" id="lastName" placeholder="Enter Last Name" 
                     value={formData.lastName} onChange={handleChange} required/>
           </div>
+          {/* Email */}
           <div class="mb-3">
             <label htmlFor="email" className="form-label">Enter Email*</label>
             <input type="email" className="form-control" name="email" id="email" placeholder="Enter Email" 
                     value={formData.email} onChange={handleChange} required/>
           </div>
+          {/* Phone */}
           <div class="mb-3">
             <label htmlFor="phone" className="form-label">Contact*</label>
             <input type="number" className="form-control" name="phone" id="phone" placeholder="Enter Mobile number" 
                     value={formData.phone} onChange={handleChange} required/>
           </div>
+          {/* Gender */}
           <div class="mb-3">
             <label class="form-label">Gender*</label>
           </div>
@@ -85,47 +117,53 @@ function App() {
                    onChange={handleChange} checked={formData.gender === 'Other'} value="Other"/>
             <label class="form-check-label" htmlFor="gender">Other</label>
           </div>
+          {/* Subject */}
           <div class="mb-3">
             <label class="form-label">Your best Subject*</label>
           </div>
           <div class="form-check form-check-inline mb-3">
             <input class="form-check-input" type="checkbox" name="subject" id="English" value="English"
-                    onChange={handleChange} checked={formData.subject === 'English'}
-            />
+                    onChange={handleChange} checked={formData.subject === 'English'} />
             <label class="form-check-label" for="inlineCheckbox1">English</label>
           </div>
           <div class="form-check form-check-inline mb-3">
             <input class="form-check-input" type="checkbox" name="subject" id="Maths" value="Maths"
-                    onChange={handleChange} checked={formData.subject === 'Maths'}
-            />
+                    onChange={handleChange} checked={formData.subject === 'Maths'} />
             <label class="form-check-label" for="Maths">Maths</label>
           </div>
           <div class="form-check form-check-inline mb-3">
             <input class="form-check-input" type="checkbox" name="subject" id="Physics" value="Physics"
-                    onChange={handleChange} checked={formData.subject === 'Physics'}
-            />
+                    onChange={handleChange} checked={formData.subject === 'Physics'} />
             <label class="form-check-label" for="Physics">Physics</label>
           </div>
+          {/* Resume */}
           <div class="mb-3">
-            <label for="formFile" class="form-label">Upload Resume*</label>
-            <input class="form-control" type="file" id="formFile"/>
+            <label htmlFor="resume" class="form-label">Upload Resume*</label>
+            <input class="form-control" type="file" name="resume" id="resume" onChange={handleChange} required/>
           </div>
+          {/* URL */}
           <div class="mb-3">
             <label for="url" class="form-label">Enter URL*</label>
-            <input type="text" class="form-control" id="url" placeholder="Enter URL"/>
+            <input type="url" class="form-control" name="url" id="url" value={formData.url} required
+                    onChange={handleChange} placeholder="Enter URL"/>
           </div>
+          {/* Choice */}
           <div class="mb-3">
-            <label for="url" class="form-label">Select your choice</label>
-              <select class="form-select" aria-label="Default select example">
+            <label for="choice" class="form-label">Select your choice</label>
+              <select class="form-select" aria-label="Default select example" name="choice" id="choice" 
+                      value={formData.choice} onChange={handleChange}>
                 <option selected>Select your ans</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
+                <option value="One">One</option>
+                <option value="Two">Two</option>
+                <option value="Three">Three</option>
               </select>
           </div>
+          {/* About */}
           <div class="mb-3">
-            <label for="url" class="form-label">About</label>
-            <textarea class="form-control" name="" id="" rows="8" placeholder="About your self"></textarea>
+            <label for="about" class="form-label">About</label>
+            <textarea class="form-control" name="about" id="about" rows="8" placeholder="About your self" required
+                      value={formData.about} onChange={handleChange}
+            />
           </div>
           <label for="url" class="form-label">Submit Or Reset</label>
           <div class="justify-content-center">
@@ -138,7 +176,17 @@ function App() {
         <ul>
           {contacts.map((contact, index) => (
             <li key={index}>
-              {contact.firstName} - {contact.lastName} - {contact.email} - {contact.phone} - {contact.gender} - {contact.subject}
+              <p>Contact Number {index+1} </p>
+              <p>First Name: {contact.firstName}</p>
+              <p>Last Name: {contact.lastName}</p>
+              <p>Email: {contact.email}</p>
+              <p>Phone: {contact.phone}</p>
+              <p>Gender: {contact.gender}</p>
+              <p>Subject: {contact.subject}</p>
+              <p>Resume : {contact.resume}</p>
+              <p>URL : <a href='{contact.url}'>{contact.url}</a></p>
+              <p>Choice : {contact.choice}</p>
+              <p>About : {contact.about}</p>
             </li>
           ))}
         </ul>
